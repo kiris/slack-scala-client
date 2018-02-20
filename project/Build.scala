4f133ba95699f3a7c3e5c38ef6103f4d8aae1d82
kiris <kiris60@gmail.com>
2018-02-20 19:47:54 +0900

    ..

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import sbtrelease._
 
 object BuildSettings {
   val buildOrganization = "com.github.gilbertw1"
-  val buildVersion      = "0.2.2"
+  val buildVersion      = "0.2.3-SNAPSHOT"
   val buildScalaVersion = "2.12.3"
   val buildCrossScalaVersions = Seq("2.11.11", "2.12.3")
 
